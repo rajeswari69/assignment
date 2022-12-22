66794ec471a19425cd914542ad23ea4eccbed3a6
rajeswari69 <rajeswarigottipalli69@gmail.com>
2022-12-23 00:18:47 +0530

    Modified the java program

--- samplejava/src/hello/demo.java
+++ samplejava/src/hello/demo.java
@@ -3,7 +3,7 @@ package hello;
 public class demo {
 	public static void main(String[] args) {
 	
-		System.out.println("Hello World");
+		System.out.println("Hello World, This is java program");
 		
 	}
 
